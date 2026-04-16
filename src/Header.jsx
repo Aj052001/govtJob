04ca29400ce627918e31f9cba91123072726e721
piyushjangir666-beep <piyushjangir666@gmail.com>
2026-04-16 13:01:24 +0530

    Rank checker

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Search, Bell, Settings, Menu, X } from "lucide-react";
+import { Link } from "react-router-dom" 
+import { Search, Bell, Settings, Menu, X } from "lucide-react" 
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false) 
 
   return (
-    <header className="w-full bg-white shadow-md">
+    <header className="w-full bg-white shadow-md sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
 
         <div className="text-xl font-bold text-blue-600">
           <Link to="/">GovCareer</Link>
         </div>
-
+ 
         <nav className="hidden md:flex gap-6 text-gray-700 font-medium ">
           <Link to="/">Home</Link>
           <Link to="/rank">Rank</Link>
-          <Link to="/books">Books</Link>
+          <Link to="/books">Books</Link>   
           <Link to="/notes">Notes</Link>
           <Link to="/typing">Typing</Link>  
           <Link to="/mock-test">Mock Test</Link>
@@ -33,7 +33,7 @@ const Header = () => {
             />
           </div>
 
-
+  
  <div className="flex gap-4 pt-2">
             <Bell />
             <Settings />
@@ -42,7 +42,7 @@ const Header = () => {
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS6xF72LSS714ihYtTOD3xjbXJAbhb5pUQDg&s"
                 alt="profile"
                 className="w-10 h-10 rounded-full border-4 border-white  object-cover shadow-md"
-              />
+              />     
           <button
             className="md:hidden"
             onClick={() => setOpen(!open)}
@@ -71,8 +71,7 @@ const Header = () => {
             <Link to="/notes" onClick={() => setOpen(false)}>Notes</Link>
             <Link to="/typing" onClick={() => setOpen(false)}>Typing</Link>
             <Link to="/mock-test" onClick={() => setOpen(false)}>Mock Test</Link>
-          </nav>
-
+          </nav> 
          
         </div>
       )}
